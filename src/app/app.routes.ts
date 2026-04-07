b3baad1c7f08a2447dbb497574ddfad5e54b7e23
DarwinFierro <darwinfrsoftware@gmail.com>
2026-04-07 18:01:19 -0500

    fix(Navbar): se limpian navegacion innecesaria

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,27 +8,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/home/home.component').then(m => m.HomeComponent),
   },
-  {
-    path: 'events',
-    loadComponent: () =>
-      import('./features/events/events-list/events-list.component').then(m => m.EventsListComponent),
-  },
-  {
-    path: 'events/:id',
-    loadComponent: () =>
-      import('./features/events/event-detail/event-detail.component').then(m => m.EventDetailComponent),
-  },
-  {
-    path: 'groups',
-    loadComponent: () =>
-      import('./features/groups/groups-list/groups-list.component').then(m => m.GroupsListComponent),
-  },
-  {
-    path: 'groups/:id',
-    loadComponent: () =>
-      import('./features/groups/group-detail/group-detail.component').then(m => m.GroupDetailComponent),
-  },
-  {
+{
     path: 'login',
     redirectTo: '',
     pathMatch: 'full',
