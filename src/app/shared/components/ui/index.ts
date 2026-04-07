ed5b2055b092318d5bbffc0e404d1717fa38d947
DarwinFierro <darwinfrsoftware@gmail.com>
2026-04-07 16:09:37 -0500

    feat(ui/Barrels): se crean los barrels para importacion de los componentes reutilizables

---
+++ src/app/shared/components/ui/index.ts
@@ -0,0 +1,5 @@
+export * from './avatar/avatar.component';
+export * from './badge/badge.component';
+export * from './button/button.component';
+export * from './input/input.component';
+export * from './spinner/spinner.component';
